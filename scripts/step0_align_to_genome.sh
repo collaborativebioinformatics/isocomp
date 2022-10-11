86bd6a9e3ca536271f53d65abf4e10cf479e470e
Chia Sin Liew <c.s.liew@huskers.unl.edu>
2022-10-11 12:22:50 -0500

    Add comment to align to genome script

--- scripts/step0_align_to_genome.sh
+++ scripts/step0_align_to_genome.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+"""
+For some background information:
+  Step 0)
+
+  With three input FASTAs (e.g. HG002, HG004, HG005), for each fasta file
+
+  (0B) Make sure FASTA seq IDs are both unique and have the
+  sample name. Proposal: "SampleName_i_existingFastaSeqID"
+  whereby i is the increment
+
+  (0C) Align these three FASTAs to the T2T reference, and
+  create a BAM file. This script does this part of step 0.
+"""
+
 t=10
 minimap2 -t "$t" -a ref_genome.fa sample_corrected.renamed.fasta > sample.sam
 samtools view -@ "$t" -bo sample.bam sample.sam
