4b522d3829267ca72f80081ab7588679790cfca6
Chia Sin Liew <c.s.liew@huskers.unl.edu>
2022-10-11 16:57:02 -0500

    Add comment and default output file to rename fasta script

--- scripts/step0_rename_fasta_desc.py
+++ scripts/step0_rename_fasta_desc.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
 
+"""
+For some background information:
+    Step 0)
+
+    With three input FASTAs (e.g. HG002, HG004, HG005), for each fasta file:
+
+    (0B) Make sure FASTA seq IDs are both unique and have the
+    sample name. Proposal: "SampleName_i_existingFastaSeqID"
+    whereby i is the increment. <- This script takes care of this part
+    of step 0.
+
+    (0C) Align these three FASTAs to the T2T reference, and
+    create a BAM file.
+
+Brief description:
+    The script takes one FASTA file, rename the sequence
+    names to "SampleName_i_existingFastaSeqID" and print the output to
+    an output FASTA file (default = <sample>.renamed.fasta, if not specified)
+
+usage:
+    step0_rename_fasta_desc.py -i input.fasta -o output.fasta -s sample1
+    step0_rename_fasta_desc.py -i input.fasta -s sample1
+"""
+
 import argparse
 import os
 
@@ -9,17 +33,13 @@ parser.add_argument(
     "-i", "--input", required=True, help="path to input FASTA file", action="store"
 )
 parser.add_argument(
-    "-o",
-    "--output",
-    required=True,
-    help="path to output renamed FASTA file",
-    action="store",
+    "-o", "--output", help="path to output renamed FASTA file", action="store",
 )
 parser.add_argument(
     "-s",
     "--sample",
     required=True,
-    help="sample name to append to sequence name",
+    help="sample name to append to sequence name, e.g. sample1",
     action="store",
 )
 
@@ -43,9 +63,12 @@ def rename_fa_desc(input_fa: str, output_fa: str, sample: str, i: int = 1):
 
 if __name__ == "__main__":
     args = parser.parse_args()
+    sample = args.sample
     input_fa = args.input
     if not os.path.exists(input_fa):
         raise SystemError("Error: %s does not exist\n" % input_fa)
-    output_fa = args.output
-    sample = args.sample
+    if not args.output:
+        output_fa = sample + ".renamed.fasta"
+    else:
+        output_fa = args.output
     rename_fa_desc(input_fa, output_fa, sample)
