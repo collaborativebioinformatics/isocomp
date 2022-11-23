7cfb40aedf7431444022fcd2e38fa889abeb90ef
Yutong Qiu <yutongq@andrew.cmu.edu>
2022-11-23 14:18:51 -0500

    trim_indel_ends with threshold

--- scripts/trim_indel_ends.py
+++ scripts/trim_indel_ends.py
@@ -4,12 +4,24 @@
     Date: Nov 20
     Description: For each pair of alinment between reads, if the best alignment includes indels at the beginning and ends
         of the reads, then trim off the indels.
-    Output: new set of reads for each sample.
+    Input: 
+        - old sets of reads for each sample
+        - aln_stats
+    Output: 
+        - new set of reads for each sample.
+
+    Trimming logic alternatives:
+        - Trim prefix and suffix regardless of the rest of the alignment --- set a threshold
+        - Trim prefix and suffix only when the rest of the alignment is all matches --- set a threshold
 """
 
 import pandas as pd
 from collections import defaultdict, Counter
 import re
+import edlib
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def extract_edits(cigar_str) -> list:
     """Given a cigar string, return separated edits with counts
@@ -37,7 +49,36 @@ def ed_from_cigar(cigar_str) -> int:
     match_count = sum([e[0] for e in all_edits if e[1] == "="])
     return total_length - match_count
 
+def get_len_type(indel_list:list) -> tuple[str, str]:
+    """Given a list of indels, return the total length and type
+
+    Args:
+        indel_list (list): list of indels
+
+    Returns:
+        tuple[str, str]: type, length
+    """
+
+    indel_type = ""
+    indel_length = 0
+    if len(indel_list) != 0:
+        for edit in indel_list:
+            if indel_type != "":
+                assert indel_type == edit[-1]
+            else:
+                indel_type = edit[-1]
+            indel_length += edit[0]
+    return indel_type, indel_length
+
 def get_prefix_and_suffix_indels(cigar_str:str) -> tuple:
+    """Get indel edits from the prefix and suffix of the cigar string
+
+    Args:
+        cigar_str (str): cigar string
+
+    Returns:
+        tuple: (list, list): prefix list of indels and suffix list of indels
+    """
     all_edits = extract_edits(cigar_str)
     prefix_indels = []
     suffix_indels = []
@@ -49,9 +90,11 @@ def get_prefix_and_suffix_indels(cigar_str:str) -> tuple:
         if all_edits[i][-1] != "I" and all_edits[i][-1] != "D":
             break
         suffix_indels.append(all_edits[i])
-    return prefix_indels, suffix_indels
 
-def trim_seq(prefix_type, prefix_len, suffix_type, suffix_len, seq, num) -> str:
+    # return prefix_indels, suffix_indels[::-1]
+    return get_len_type(prefix_indels), get_len_type(suffix_indels[::-1])
+
+def trim_seq(prefix_type, prefix_len, suffix_type, suffix_len, seq, num, threshold=10) -> str:
     """Trim input sequence according to suffix and prefix number of indels
 
     Args:
@@ -62,25 +105,26 @@ def trim_seq(prefix_type, prefix_len, suffix_type, suffix_len, seq, num) -> str:
         seq (str): input sequence to be trimmed
         num (int): ranking of the sequence. If a sequence is the first in alignment, deletion means that removing. 
         Otherwise, insertion means trimming.
+        threshold (int): maximum number of bases trimmed
 
     Returns:
         str: trimmed sequence
     """
     assert num == 1 or num == 2, "trim_seq(): unknown ranking of input sequence!"
 
-    if num == 1:
+    if num == 2:
         if prefix_type == "D":
-            seq = seq[prefix_len:]
+            seq = seq[min(threshold, prefix_len):]
         if suffix_type == "D":
-            seq = seq[:-suffix_len]
+            seq = seq[:-min(threshold, suffix_len)]
     if num == 1:
         if prefix_type == "I":
-            seq = seq[prefix_len:]
+            seq = seq[min(threshold, prefix_len):]
         if suffix_type == "I":
-            seq = seq[:-suffix_len]
+            seq = seq[:-min(threshold, suffix_len)]
     return seq
 
-def trim_from_cigar(cigar_str, sequence1, sequence2):
+def trim_from_cigar(cigar_str, sequence1, sequence2, threshold=10):
     """Given two sequences and the cigar string describing the alignment between them, trim the start and end, if the 
     alignment starts and/or ends with indels.
 
@@ -88,135 +132,167 @@ def trim_from_cigar(cigar_str, sequence1, sequence2):
         cigar_str (str): cigar_string
         sequence1(str): aligned sequence 1 to be trimmed
         sequence2(str): aligned sequence 2 to be trimmed
+        threshold(int): maximum number of bases trimmed
     Returns:
         str: trimmed sequence 1
         str: trimmed sequence 2
     """
 
-    prefix_indels, suffix_indels = get_prefix_and_suffix_indels(cigar_str)
+    (prefix_type, prefix_length), (suffix_type, suffix_length) = get_prefix_and_suffix_indels(cigar_str)
 
-    if len(prefix_indels) == 0 and len(suffix_indels) == 0:
+    if prefix_length == 0 and suffix_length == 0:
         return sequence1, sequence2
+
     global total_trim
     total_trim += 1
-    # print("Prefix and suffix indels:", prefix_indels, suffix_indels)
 
-    # print("Before trimming:", len(sequence1), len(sequence2))
-
-    prefix_type = ""
-    prefix_length = 0
-    if len(prefix_indels) != 0:
-        for edit in prefix_indels:
-            if prefix_type != "":
-                assert prefix_type == edit[-1]
-            else:
-                prefix_type = edit[-1]
-            prefix_length += len(edit[:-1])
-
-    suffix_type = ""
-    suffix_length = 0
-    if len(suffix_indels) != 0:
-        for edit in suffix_indels:
-            if suffix_type != "":
-                assert suffix_type == edit[-1]
-            else:
-                suffix_type = edit[-1]
-            suffix_length += len(edit[:-1])
-
-    sequence1 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence1, 1)
-    sequence2 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence2, 2)
+    sequence1 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence1, 1, threshold)
+    sequence2 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence2, 2, threshold)
 
     global correct_trim
-    if len(sequence1) == len(sequence2):
+    if len(sequence1) == len(sequence2) and sequence1 == sequence2:
         correct_trim += 1
-
-    # print("After trimming:", len(sequence1), len(sequence2))
 
     return sequence1, sequence2
 
     
+def read_fasta(fname:str, sample_name:str, seq_dict:dict) -> dict:
+    """Parse fasta file. add sequences to input sequence dict
+
+    Args:
+        fname (str): bam file name
+        sample_name(str): sample name
+        seq_dict(dict): input and output dict
+
+    Returns:
+        dict: dictionary matching read name to sequence
+    """
+    fasta_sequences = SeqIO.parse(open(fname),'fasta')
+    for fasta in tqdm(fasta_sequences):
+        name, sequence = fasta.id, str(fasta.seq)
+        seq_dict[sample_name + "_" + name] = sequence
+    return seq_dict
 
 total_trim = 0
-
-input_dir = "/Users/yutongq/SVHackathon2022/"
-fname = input_dir+"unique_isoforms_and_aln_stats.tsv"
-
-
-"""Header
-    win_chr
-    win_start
-    win_end
-    total_isoform
-    isoform_name
-    sample_from
-    sample_compared_to
-    mapped_start
-    isoform_sequence
-    selected_alignments
-"""
-aln_stats_df = pd.read_csv(fname, sep="\t").dropna()
-
-'''
-    For each sample:
-        for each read in sample:
-            will there be multiple reads that are associated with each other?
-'''
-
-records = aln_stats_df.to_dict("records")
-
-unique_read_pairs = defaultdict(dict)             # {read_id1: {read_id2: edit distance}}
-read_pair_cigar = defaultdict(dict)               # {read_id1: {read_id2: cigar_string}}
-read_sequence = dict()                            # {read_id: sequence}
-
-for record in records:
-    read_id = record["sample_from"] + "_" + record["isoform_name"]
-    compared_to= "_".join(record["selected_alignments"].split("_")[1:3])
-    cigar = record["selected_alignments"].split("_")[-1]
-
-    read_sequence[read_id] = record["isoform_sequence"]
-
-    # # check if read_id, compared_to pair is already accounted for.
-    # if compared_to in unique_read_pairs and read_id in unique_read_pairs[compared_to]:
-    #     continue
-
-    if compared_to not in unique_read_pairs[read_id]:
-        read_pair_cigar[read_id][compared_to] = cigar
-        unique_read_pairs[read_id].add(compared_to)
-
-multi_pair = Counter()
-unique_pair_cigar = defaultdict(dict)
-
 correct_trim = 0
 
-# print(read_pair_cigar)
-for read_id in unique_read_pairs:
-    multi_pair[len(unique_read_pairs[read_id])] += 1
+def main():
+    input_dir_aln = "/Users/yutongq/SVHackathon2022/isocomp/data/"
+    input_dir_fasta = "/Users/yutongq/SVHackathon2022/"
 
-    if len(unique_read_pairs[read_id]) == 1:
-        for compared_to in unique_read_pairs[read_id]:
-                cigar = read_pair_cigar[read_id][compared_to]
-                seq1 = read_sequence[read_id]
-                try:
-                    seq2 = read_sequence[compared_to]
-                except KeyError:
-                    break
-                new_seq1, new_seq2 = trim_from_cigar(cigar, seq1, seq2)
-                old_ed = ed_from_cigar(read_pair_cigar[read_id][compared_to])
-                ## TODO new_ed = 
-                break
+    aln_fname = input_dir_aln+"unique_isoforms_and_aln_stats.tsv"
+    fasta_1_fname = input_dir_fasta+"HG002_MM2_corrected.fasta"
+    fasta_2_fname = input_dir_fasta+"HG004_MM2_corrected.fasta"
+    fasta_3_fname = input_dir_fasta+"HG005_MM2_corrected.fasta"
 
+    read_sequence = dict()
 
-print("total trim:", total_trim)
-print("Correct trim:", correct_trim)
+    print("Parsing fasta files...")
+    read_sequence = read_fasta(fasta_1_fname, "HG002", read_sequence)
+    read_sequence = read_fasta(fasta_2_fname, "HG004", read_sequence)
+    read_sequence = read_fasta(fasta_3_fname, "HG005", read_sequence)
 
+    aln_stats_df = pd.read_csv(aln_fname, sep="\t").dropna()
+    aln_records = aln_stats_df.to_dict("records")
+
+    unique_read_pairs = defaultdict()
+    unique_read_pairs_ed = defaultdict(dict)          # {read_id1: {read_id2: edit distance}}
+    read_pair_cigar = defaultdict(dict)               # {read_id1: {read_id2: cigar_string}}
+
+    print("Parsing read comparisons...")
+    for record in tqdm(aln_records):
+        read_id = record["sample_from"] + "_" + record["isoform_name"]
+        selected_alignments = record["selected_alignments"].split(",")
+        for alignment in selected_alignments:
+            compared_to= alignment.split("__")[1]
+
+            # # store a pair only once
+            if compared_to in unique_read_pairs_ed and read_id in unique_read_pairs_ed[compared_to]:
+                continue
+
+            cigar = alignment.split("_")[-1]
+
+            read_pair_cigar[read_id][compared_to] = cigar
+            ed = edlib.align(read_sequence[read_id], read_sequence[compared_to])["editDistance"]
+            unique_read_pairs_ed[read_id][compared_to] = ed
+
+            # if a read is matched with multiple reads, keep only the most similar pair
+            if read_id in unique_read_pairs:
+                opt_compared_to = unique_read_pairs[read_id]
+                if ed < unique_read_pairs_ed[read_id][opt_compared_to]:
+                    unique_read_pairs[read_id] = compared_to
+            else:
+                unique_read_pairs[read_id] = compared_to
+
+    print("Trimming...")
+
+    old_eds = []
+    new_eds = []
+    length_diff1 = []
+    length_diff2 = []
+
+    processed_read = set()
+    for read_id in tqdm(unique_read_pairs):
+        compared_to = unique_read_pairs[read_id]
+        cigar = read_pair_cigar[read_id][compared_to]
+
+        seq1 = read_sequence[read_id]
+        seq2 = read_sequence[compared_to]
+        new_seq1, new_seq2 = trim_from_cigar(cigar, seq1, seq2, threshold=100000)
+        old_ed = ed_from_cigar(read_pair_cigar[read_id][compared_to])
+        new_align = edlib.align(new_seq1, new_seq2,mode = "NW", task = "path" )
+        new_ed = new_align['editDistance']
+        new_cigar = new_align["cigar"]
+
+        if seq1 != new_seq1 or seq2 != new_seq2:
+            old_eds.append(old_ed)
+            new_eds.append(new_ed)
+            if new_ed == 0:
+                length_diff1.append(abs(len(seq1) - len(new_seq1)))
+                length_diff1.append(abs(len(seq2) - len(new_seq2)))
+            else:
+                length_diff2.append(abs(len(seq1) - len(new_seq1)))
+                length_diff2.append(abs(len(seq2) - len(new_seq2)))
+        processed_read.add(read_id)
+        processed_read.add(compared_to)
 
     
+    # for read_id in read_sequence:
+    #     if read_id not in processed_read:
+    #         print(read_id)
 
-    # if len(unique_read_pairs[r]) > 1:
-    #     eds = []
-    #     for compared_to in unique_read_pairs[r]:
-    #         print(r,compared_to+",")
-    #         ed = ed_from_cigar(read_pair_cigar[r][compared_to])
-    #         eds.append(ed)
-    #     print(eds)
-print("Multi pair", multi_pair)
+        # print("Length:", len(seq1), len(seq2), len(new_seq1), len(new_seq2))
+
+    print("Total reads:", len(read_sequence))
+    print("Total pairs:", len(unique_read_pairs))
+    print("Total trim:", total_trim)
+    print("Correct trim:", correct_trim)
+
+    fig, ax = plt.subplots(1)
+    ax.scatter(old_eds, new_eds, s=5)
+    ax.plot([0, max(old_eds)], [0, max(old_eds)], color="red")
+    ax.set_xlabel("original edit distance")
+    ax.set_ylabel("timmed edit distance")
+    ax.set_title("Edit distance before and after trimming")
+    fig.savefig("ED_trim.png")
+
+
+    fig,ax = plt.subplots(1)
+    ax.hist(length_diff1, range=[0,100],bins=100)
+    ax.hist(length_diff2, range=[0,100],bins=100, alpha=0.5)
+
+    fig.savefig("ED_trim_length.png")
+
+if __name__ == "__main__":
+    main()
+
+    # seq1="GTCTTTCCTCATTTACAGCTAGCTCTGCCAGTCTGTGTCTTTGCTGTGTCTGTGGATGTGAGTGTCCCTGCTGGTCTGTAGGAGATGGTATTTTGGGGGCAGCTGCAAGGGAAAAAACTGATCTGCTACTTACACAGCGCCGTAGCCTCAGCCTGAGGGTCTTCAGGTTCTCCCCCAGGGAGTTCACATGCGCCTTGATGTCTGGGTCTTGGTTCTCAGCTTGGGGCATCACCTCCTCCAGGTAAAACTGGATCATCTCAGACAAGGCTTGGCAACCCAGGTAACCCTAAGGGCAGGAGCCAAAGAGCGGTGCTTGCACACACTGACAGGAGTCCAAGAATGTGCACTGAGGGAGCGTTTCCGCACAGATCTGCGTGTTCCTTACCACTCACACATGTGCACACACATATCCATGTGTGTGTGCCAGTGCTTTGGGGCTCTGTTCCACGGGGTGCAAACCCTAGACGAAAAAAGACTGTAATTCAACTCTTCAGTAGCCAGGATTGCCTGTCTCCAACCCCTTCTGAATCTAGTTATGTTGGATACTGGGAGAGAAGCATGTAGACTCTTTGGGCCTGACCTTTCTGATTTTTCTCTGGAAGGGCTTCACTGAAGGGGCACAGAAGAATAATGTTTCTACAGCATAGATCCAGGGGCTCCTCCCCACCAGGAACCCAGCTTAGCAGGCAGGCTGTTCATGTGGAACTCGCCAAAACCCCGTGCAGGTGGATGTGCGGAAACACCAAAGAACTGCACGATTGTCCCTGCCTTTGGAAGGTCTCATTCCCTGTCATTCCTTCATTCAGTCAACAAACTTGGAGGACCCAGTATGTGCTACATTTTGAGGATAAAGTGATGAACAACACAGACAAGATCCCTGCCCTTGTGGAGTCTATATTCAAACCAGAGCTTGACCTTCAAACTGAGAGACCAACTTAAAAATGGGGCCATCTGGCCACTAACTGTTATACACCCTCTGCTTGGGACACACGGTCTCAGCCCTCCTCCACCTTCAAGTCTGATAGGAGAGAGATTTCAGAGTGGCCCAAACAATCTAAAGTAAAAGTGAACAGAGTTCAAAATAAGTCCTCAAGGGCTGAGGGGAGACCAAGGGGAAGATAATGCGGTGAGGGCACTGAGAGAGGGATTCCTAGAGGAATTGTACCTTGCTTTGAATTTTAGCCCTAAGCTGCAAATCCTAGCTAACTTCTAAGTAGATGCTGACAGCTGGCTTCTATTTGGTCTGCCATATTGAATGGCTTTGTCCCCAGGACATGGTGGAGATTTAGACAACCCAGGGCAGAAAATCCTCTAGGAAAGGAAGTCTGAACTACACCGCTGGCTTTCTGAAAGGACAGGATTTGAGGAGCTGGGATGGTAGTAGGGGAGAGGGCAGCTGGGTGGGGTTTCTTCCAGGAAACAATGTGGATGCAAGATGCAGTCCTCGTCCCTGGCCTGCCCTTACCGCTTTAGTATTTCAGATGAGTCAGCTCTCACTCTGCCACCTGCTTTCTGTAACTCTGCAGCAGGCCAGCAGCCTCCAGTCCACCCTCTCCTCCTCCCAGGGCTCTGGGGTGAAGCCCTCCCCAGGAACTCCCTGGCTCCCAGTACCCATGGGAGAAGCTCTTTTCAACTGACATGATTTATTTTAACAATAAGAGGAAAAAACCACATCCGATTTCCACAGAATTAGAGTTCCTCCTGACACGCCTGGACTTGCCAGTTGGGAAGGACACTCTCAAGCCATCCTCTATGCCATTTGCCGTTCTGCTGTTCCTTTGCTTGGAGGGAAGAGATGCCTGTTTCTATAGGGAGGAAGTTTGAAGTGAGAAGAGAGGACAAAGATTCTGTGAATGGAAAGAGCACTGGACTGAGAGTTCAGGGATGTGGCCTCCGGTCCCAGCTCCACCACTACTCAGTTGTCTGACGAAGGAAACAGAAAGTTGTGATACAATGTGGGTGACACATTGCCTAGAAAGAAGTGCCGTTGTGATACGCTTATGTTGGTGGGATGGGGGAAAGAAATAACAGGTTTGTATGGAGTGTTATGAAAGAATTAAATCTTAACCTTCCATTGGGGTAAGCTGATGGAAACAACCATAGCAAAGGACAGACTGAATATTTTTTTATTCTCTTTATAGAAAATAACAGTAAAAAAAAGTATTGACATAGGAGAAGGAAACAAAAAGTTGTCAGGAGTTAATGAATAGAAATATTATTTTTTTCTGGATTTTATGGTGTTTGTGTTATTTGTTAGCTTTTATGAATTTGTAATTTGTTGTAATTTCTTTCTAAATAAGTATTTACTTTTGTCTCT"
+
+    # seq2 = "TAGCTCTGCCAGTCTGTGTCTTTGCTGTGTCTGTGGATGTGAGTGTCCCTGCTGGTCTGTAGGAGATGGTATTTTGGGGGCAGCTGCAAGGGAAAAAACTGATCTGCTACTTACACAGCGCCGTAGCCTCAGCCTGAGGGTCTTCAGGTTCTCCCCCAGGGAGTTCACATGCGCCTTGATGTCTGGGTCTTGGTTCTCAGCTTGGGGCATCACCTCCTCCAGGTAAAACTGGATCATCTCAGACAAGGCTTGGCAACCCAGGTAACCCTAAGGGCAGGAGCCAAAGAGCGGTGCTTGCACACACTGACAGGAGTCCAAGAATGTGCACTGAGGGAGCGTTTCCGCACAGATCTGCGTGTTCCTTACCACTCACACATGTGCACACACATATCCATGTGTGTGTGCCAGTGCTTTGGGGCTCTGTTCCACGGGGTGCAAACCCTAGACGAAAAAAGACTGTAATTCAACTCTTCAGTAGCCAGGATTGCCTGTCTCCAACCCCTTCTGAATCTAGTTATGTTGGATACTGGGAGAGAAGCATGTAGACTCTTTGGGCCTGACCTTTCTGATTTTTCTCTGGAAGGGCTTCACTGAAGGGGCACAGAAGAATAATGTTTCTACAGCATAGATCCAGGGGCTCCTCCCCACCAGGAACCCAGCTTAGCAGGCAGGCTGTTCATGTGGAACTCGCCAAAACCCCGTGCAGGTGGATGTGCGGAAACACCAAAGAACTGCACGATTGTCCCTGCCTTTGGAAGGTCTCATTCCCTGTCATTCCTTCATTCAGTCAACAAACTTGGAGGACCCAGTATGTGCTACATTTTGAGGATAAAGTGATGAACAACACAGACAAGATCCCTGCCCTTGTGGAGTCTATATTCAAACCAGAGCTTGACCTTCAAACTGAGAGACCAACTTAAAAATGGGGCCATCTGGCCACTAACTGTTATACACCCTCTGCTTGGGACACACGGTCTCAGCCCTCCTCCACCTTCAAGTCTGATAGGAGAGAGATTTCAGAGTGGCCCAAACAATCTAAAGTAAAAGTGAACAGAGTTCAAAATAAGTCCTCAAGGGCTGAGGGGAGACCAAGGGGAAGATAATGCGGTGAGGGCACTGAGAGAGGGATTCCTAGAGGAATTGTACCTTGCTTTGAATTTTAGCCCTAAGCTGCAAATCCTAGCTAACTTCTAAGTAGATGCTGACAGCTGGCTTCTATTTGGTCTGCCATATTGAATGGCTTTGTCCCCAGGACATGGTGGAGATTTAGACAACCCAGGGCAGAAAATCCTCTAGGAAAGGAAGTCTGAACTACACCGCTGGCTTTCTGAAAGGACAGGATTTGAGGAGCTGGGATGGTAGTAGGGGAGAGGGCAGCTGGGTGGGGTTTCTTCCAGGAAACAATGTGGATGCAAGATGCAGTCCTCGTCCCTGGCCTGCCCTTACCGCTTTAGTATTTCAGATGAGTCAGCTCTCACTCTGCCACCTGCTTTCTGTAACTCTGCAGCAGGCCAGCAGCCTCCAGTCCACCCTCTCCTCCTCCCAGGGCTCTGGGGTGAAGCCCTCCCCAGGAACTCCCTGGCTCCCAGTACCCATGGGAGAAGCTCTTTTCAACTGACATGATTTATTTTAACAATAAGAGGAAAAAACCACATCCGATTTCCACAGAATTAGAGTTCCTCCTGACACGCCTGGACTTGCCAGTTGGGAAGGACACTCTCAAGCCATCCTCTATGCCATTTGCCGTTCTGCTGTTCCTTTGCTTGGAGGGAAGAGATGCCTGTTTCTATAGGGAGGAAGTTTGAAGTGAGAAGAGAGGACAAAGATTCTGTGAATGGAAAGAGCACTGGACTGAGAGTTCAGGGATGTGGCCTCCGGTCCCAGCTCCACCACTACTCAGTTGTCTGACGAAGGAAACAGAAAGTTGTGATACAATGTGGGTGACACATTGCCTAGAAAGAAGTGCCGTTGTGATACGCTTATGTTGGTGGGATGGGGGAAAGAAATAACAGGTTTGTATGGAGTGTTATGAAAGAATTAAATCTTAACCTTCCATTGGGGTAAGCTGATGGAAACAACCATAGCAAAGGACAGACTGAATATTTTTTTATTCTCTTTATAGAAAATAACAGTAAAAAAAAGTATTGACATAGGAGAAGGAAACAAAAAGTTGTCAGGAGTTAATGAATAGAAATATTATTTTTTTCTGGATTTTATGGTGTTTGTGTTATTTGTTAGCTTTTATGAATTTGTAATTTGTTGTAATTTCTTTCTAAATAAGTATTTACTTTTGTCTCTAATTTTGTCCATCTATTTTTGAATTCAATTTTCAAATTCAAAACGCTTCTCTCGGCCGGGCACGGTGGCTCACGCCTGTAATCCCAGCACTTTGGGAGGTCGAGGAGGGCGGATCACGAGGTCAGGAGATCGAGACCATCCTGGCTAACACAGTGAAACCCCGTCTCTACTAAAAATACAAAAAATTAGCCAGGCGAGGTGGTGGGCGCCTGTAGTCCCAGCTACTCGGGAGGCTGAGGCAGGAGAATGGCATGAACCCCGGGGGGCGGAGCCTGCAGTGAGCCGAGATCGTGCCACTGCACTCCAGCCTGGGTGACAGCGAGACTCCGTCTC"
+
+    # cigar = "1I1=8I1=6I3=2I2=2I2291=333D"
+
+    # new_seq1, new_seq2 = trim_from_cigar(cigar, seq1, seq2, 1000)
+
+    # print(len(seq1), len(seq2))
+    # print(len(new_seq1), len(new_seq2))
