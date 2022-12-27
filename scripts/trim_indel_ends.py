33c05342e9557c96409d7006ccf29df1d8d3c720
Yutong Qiu <yutongq@andrew.cmu.edu>
2022-12-27 13:04:36 -0500

    replaced print with logger and global variables with trimTracker class

--- scripts/trim_indel_ends.py
+++ scripts/trim_indel_ends.py
@@ -28,6 +28,13 @@ import os
 import sys
 import argparse
 import datetime
+import logging
+
+logging.basicConfig()
+logging.root.setLevel(logging.CRITICAL)
+
+logging_handle = "trim_indel_ends"
+logger = logging.getLogger(logging_handle)
 
 #--------------------------------------------------------
 # parser
@@ -35,7 +42,7 @@ import datetime
 # positional/optional arguments
 posList = ['manifest','unique_isoform_aln']
 optList = ['mode', 'threshold']
-sys.stdout.write('\n'); sys.stdout.flush()
+print()
 
 # author and version info
 usage = sys.argv[0] + ' <options> ' + ' '.join(posList) + '\n'
@@ -53,53 +60,82 @@ parser.add_argument('-t', '--'+optList[1], type=int, metavar='', default=10000, 
 
 # strict positional/optional arguments checking
 argsDict = vars(parser.parse_args())
-sys.stdout.write('\n' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - ' + sys.argv[0] + ' - Parsing Input Arguements...\n\n'); sys.stdout.flush()
+logger.critical('\n' + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - ' + sys.argv[0] + ' - Parsing Input Arguements...\n\n')
 for key, value in argsDict.items():
-    if key in posList: sys.stdout.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - Required Argument - ' + key +': '+ str(value) + '\n'); sys.stdout.flush()
-    if key in optList: sys.stdout.write(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - Optional Argument - ' + key +': '+ str(value) + '\n'); sys.stdout.flush()
+    if key in posList: logger.critical(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - Required Argument - ' + key +': '+ str(value) + '\n')
+    if key in optList: logger.critical(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + ' - Optional Argument - ' + key +': '+ str(value) + '\n')
     vars()[key] = value # assign values of arguments into shorthand global variables
-sys.stdout.write('\n'); sys.stdout.flush()
+logger.critical('\n')
 
-def extract_edits(cigar_str) -> list:
-    """Given a cigar string, return separated edits with counts
+class TrimTracker:
+    """
+    Class for tracking the number of trims done
+
+    Member variables:
+        total_trim (int) --- total number of trimming performed
+        effective_trim (int) --- number of trims that result in equality between strings
+    """
+    def __init__(self):
+        self.total_trim = 0
+        self.effective_trim = 0
+
+def extract_edits(cigar_str:str):
+    """  Given a cigar string, return separated edits with counts
 
     Args:
-        cigar_str (str):
+        cigar_str (str): Input cigar string
 
     Returns:
-        list: a list of tuples of (count, edit_type)
+        all_edits (list): a list of tuples of (count, edit_type)
+
+    Example:
+        Input: 10=5I2D3X
+        Output: [(10, '='), (5, 'I'), (2, 'D'), (3, 'X')]
     """
+
     all_edits = [(int(s[:-1]), s[-1]) for s in re.findall(r'\d+[=,I,D,X]', cigar_str)]
     return all_edits
 
-def ed_from_cigar(cigar_str) -> int:
+def ed_from_cigar(cigar_str: str) -> int:
+
     """Compute edit distance from cigar strings
 
     Args:
         cigar_str (str): cigar string
 
     Returns:
-        int: edit distance that is equal to total length of the string minus the nu
+        edit_distance (int): edit distance that is equal to total length of the string minus the number of matched characters
+
+    Example: 
+        Input:10=5I2D3X
+        Output: 10 (total length = 20, total matched = 10)
     """
+    
     all_edits = extract_edits(cigar_str)
     total_length = sum([e[0] for e in all_edits])
     match_count = sum([e[0] for e in all_edits if e[1] == "="])
     return total_length - match_count
 
 def get_len_type(indel_list:list) -> tuple[str, str]:
-    """Given a list of indels, return the total length and type
+    """Given a list of indels, return the total length and type. This function concatenates indels of the same type. Just in case.
 
     Args:
         indel_list (list): list of indels
 
     Returns:
         tuple[str, str]: type, length
+
+    Example:
+        Input: 3D2D
+        Output: "D",5
     """
 
     indel_type = ""
     indel_length = 0
     if len(indel_list) != 0:
         for edit in indel_list:
+
+            # make sure that all indels are of the same type
             if indel_type != "":
                 assert indel_type == edit[-1]
             else:
@@ -115,23 +151,38 @@ def get_prefix_and_suffix_indels(cigar_str:str, mode="lenient") -> tuple:
 
     Returns:
         tuple: (list, list): prefix list of indels and suffix list of indels
+
+    Example:
+        Input1: 5D3X2=1I, "lenient"
+        Output1: ("D", 5), ("I", 1)
+
+        Input2: 5D3X2=1I, "strict"
+        Output2: ("", 0), ("", 0)
     """
     all_edits = extract_edits(cigar_str)
     prefix_indels = []
     suffix_indels = []
     i = 0
+    # investigate prefix of the cigar string
     for i in range(len(all_edits)):
+        # break out of the loop if the current edit is no longer an INDEL
         if all_edits[i][-1] != "I" and all_edits[i][-1] != "D":
             break
         prefix_indels.append(all_edits[i])
+
+    # investigate suffix of the cigar string
     j = len(all_edits)-1
     for j in range(len(all_edits)-1, -1, -1):
+        # break out of the loop if the current edit is no longer an INDEL
         if all_edits[j][-1] != "I" and all_edits[j][-1] != "D":
             break
         suffix_indels.append(all_edits[j])
-    for idx in range(i,j+1):
-        if all_edits[idx][-1] != "=" and mode == "strict":
-            return ("", 0), ("",0)
+
+    # under strict mode, do not trim if the sequences were not matched exactly
+    if mode == "strict":
+        for idx in range(i,j+1):
+            if all_edits[idx][-1] != "=":
+                return ("", 0), ("",0)
 
     # return prefix_indels, suffix_indels[::-1]
     return get_len_type(prefix_indels), get_len_type(suffix_indels[::-1])
@@ -145,12 +196,20 @@ def trim_seq(prefix_type, prefix_len, suffix_type, suffix_len, seq, num, thresho
         suffix_type (str):type of edits at the end of the cigar, ("I" or "D")
         suffix_len (int): length of suffix edits
         seq (str): input sequence to be trimmed
-        num (int): ranking of the sequence. If a sequence is the first in alignment, deletion means that removing. 
+        num (int): The order of the sequence in alignment. If a sequence is the first in alignment, deletion means that a character is removed. 
         Otherwise, insertion means trimming.
         threshold (int): maximum number of bases trimmed
 
     Returns:
         str: trimmed sequence
+
+    Example:
+        Input1: trim_seq("D", 1, "I",2, "ATCGAAG", 1)
+        Output1: "TCGAAG"  (The first character is trimmed but the suffix is not trimmed)
+        
+        Input2: trim_seq("D", 1, "I",2, "ATCGAAG", 2)
+        Output2: "ATCGA"  (The suffix of length 2 is trimmed but the prefix is not trimmed)
+
     """
     assert num == 1 or num == 2, "trim_seq(): unknown ranking of input sequence!"
 
@@ -166,7 +225,7 @@ def trim_seq(prefix_type, prefix_len, suffix_type, suffix_len, seq, num, thresho
             seq = seq[:-min(threshold, suffix_len)]
     return seq
 
-def trim_from_cigar(cigar_str, sequence1, sequence2, threshold=10, mode="lenient"):
+def trim_from_cigar(cigar_str, sequence1, sequence2, threshold=10, mode="lenient", trim_tracker=None):
     """Given two sequences and the cigar string describing the alignment between them, trim the start and end, if the 
     alignment starts and/or ends with indels.
 
@@ -188,15 +247,14 @@ def trim_from_cigar(cigar_str, sequence1, sequence2, threshold=10, mode="lenient
     if prefix_length == 0 and suffix_length == 0:
         return sequence1, sequence2
 
-    global total_trim
-    total_trim += 1
+    if trim_tracker != None:
+        trim_tracker.total_trim += 1
 
     sequence1 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence1, 1, threshold)
     sequence2 = trim_seq(prefix_type, prefix_length, suffix_type, suffix_length, sequence2, 2, threshold)
 
-    global effective_trim
-    if len(sequence1) == len(sequence2) and sequence1 == sequence2:
-        effective_trim += 1
+    if trim_tracker != None and len(sequence1) == len(sequence2) and sequence1 == sequence2:
+        trim_tracker.effective_trim += 1
 
     return sequence1, sequence2
 
@@ -218,14 +276,8 @@ def read_fasta(fname:str, sample_name:str, seq_dict:dict) -> dict:
         name, sequence = fasta.id, str(fasta.seq)
         seq_dict[sample_name + "_" + name] = sequence
         count += 1
-    print("Read %i sequences in sample %s" % (count, sample_name))
+    logger.critical("Read %i sequences in sample %s" % (count, sample_name))
     return seq_dict
-
-# total number of pairs of sequences getting trimmed
-total_trim = 0
-
-# total number of pairs of sequences that are identical after getting trimmed
-effective_trim = 0
 
 def parseManifest(manifest:str) -> dict:
     """Parse Manifest file
@@ -236,7 +288,7 @@ def parseManifest(manifest:str) -> dict:
     Returns:
         dict: read_sequence: {read_id: sequence}
     """
-    print("Parsing fasta files...")
+    logger.critical("Parsing fasta files...")
 
     # read manifest
     with open(manifest) as f: fields = [ l.strip().split(',') for l in f ]
@@ -247,7 +299,15 @@ def parseManifest(manifest:str) -> dict:
         read_sequence = read_fasta(fa, sample, read_sequence)
     return read_sequence
 
-def write_fasta(processed_read, read_sequence, mode, threshold):
+def write_fasta(processed_read: dict, read_sequence: dict, mode: str, threshold: int):
+    """Writes the processed reads into a fasta file into sample_name_trimmed_mode_threshold.fasta for each sample in read_sequence
+
+    Args:
+        processed_read (dict): a dictionary of trimmed reads accessible by read name
+        read_sequence (dict): a dictionary of original reads accessible by read name
+        mode (str): lenient or strict
+        threshold (int): threshold for trimming
+    """
     records = defaultdict(list)
     for read_id in read_sequence:
         sample_name = read_id.split("_")[0]
@@ -263,8 +323,8 @@ def write_fasta(processed_read, read_sequence, mode, threshold):
     for sample_name in records:
         with open("%s_trimmed_%s_%s.fasta" % (sample_name, mode, str(threshold)), "w") as out:
             SeqIO.write(records[sample_name], out, "fasta")
-        print("%i sequences in %s" % (len(records[sample_name]), sample_name))
-        print("Trimmed written to %s_trimmed_%s_%s.fasta" % (sample_name, mode, str(threshold)))
+        logger.critical("%i sequences in %s" % (len(records[sample_name]), sample_name))
+        logger.critical("Trimmed written to %s_trimmed_%s_%s.fasta" % (sample_name, mode, str(threshold)))
 
 def main(manifest, unique_isoform_aln, mode, threshold):
 
@@ -276,7 +336,9 @@ def main(manifest, unique_isoform_aln, mode, threshold):
     unique_read_pairs_ed = defaultdict(dict)          # {read_id1: {read_id2: edit distance}}
     read_pair_cigar = defaultdict(dict)               # {read_id1: {read_id2: cigar_string}}
 
-    print("Parsing read comparisons...")
+    trim_tracker = TrimTracker()
+
+    logger.critical("Parsing read comparisons...")
     for record in tqdm(aln_records):
         read_id = record["sample_from"] + "_" + record["isoform_name"]
 
@@ -321,7 +383,7 @@ def main(manifest, unique_isoform_aln, mode, threshold):
             else:
                 unique_read_pairs[compared_to] = read_id
 
-    print("Trimming...")
+    logger.critical("Trimming...")
 
     old_eds = []
     new_eds = []
@@ -348,7 +410,7 @@ def main(manifest, unique_isoform_aln, mode, threshold):
 
         seq1 = read_sequence[read_id]
         seq2 = read_sequence[compared_to]
-        new_seq1, new_seq2 = trim_from_cigar(cigar, seq1, seq2, threshold=threshold, mode=mode)
+        new_seq1, new_seq2 = trim_from_cigar(cigar, seq1, seq2, threshold=threshold, mode=mode, trim_tracker=trim_tracker)
         old_ed = ed_from_cigar(read_pair_cigar[read_id][compared_to])
         new_align = edlib.align(new_seq1, new_seq2,mode = "NW", task = "path" )
         new_ed = new_align['editDistance']
@@ -372,10 +434,10 @@ def main(manifest, unique_isoform_aln, mode, threshold):
     
     write_fasta(processed_read, read_sequence, mode, threshold)
 
-    print("Total reads:", len(read_sequence))
-    print("Total pairs:", len(unique_read_pairs))
-    print("Total trim:", total_trim)
-    print("Correct trim:", effective_trim)
+    logger.critical("Total reads: %i" % len(read_sequence))
+    logger.critical("Total pairs: %i" % len(unique_read_pairs))
+    logger.critical("Total trim: %i" % trim_tracker.total_trim)
+    logger.critical("Trims that result in equality: %i" % trim_tracker.effective_trim)
 
     fig, ax = plt.subplots(1)
     ax.scatter(old_eds, new_eds, s=5)
@@ -384,7 +446,7 @@ def main(manifest, unique_isoform_aln, mode, threshold):
     ax.set_ylabel("timmed edit distance")
     ax.set_title("Edit distance before and after trimming")
     fig.savefig("ED_trim_scatter_%s_%s.png" % (mode, str(threshold)))
-    print("Scatterplot saved to ED_trim_%s_%s.png" % (mode, str(threshold)))
+    logger.critical("Scatterplot saved to ED_trim_%s_%s.png" % (mode, str(threshold)))
 
 
     fig,ax = plt.subplots(1)
@@ -396,7 +458,7 @@ def main(manifest, unique_isoform_aln, mode, threshold):
     ax.legend()
 
     fig.savefig("ED_trim_length_distribution_%s_%s.png" % (mode, str(threshold)))
-    print("Histogram saved to ED_trim_length_%s_%s.png" % (mode, str(threshold)))
+    logger.critical("Histogram saved to ED_trim_length_%s_%s.png" % (mode, str(threshold)))
 
 if __name__ == "__main__":
     main(manifest, unique_isoform_aln, mode, threshold)
